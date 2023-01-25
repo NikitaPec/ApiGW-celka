b4754688e95b39a07dc68317e92774148d719c2e
NikPecherkin <Nikpecherkin@gmail.com>
2023-01-25 20:52:37 +0300

    commit validateMiddleWare

--- middleWare/validateMiddleWare.js
+++ middleWare/validateMiddleWare.js
@@ -1,27 +1,34 @@
-import ApiError from '../exception/ApiError.js';
-import User from '../model/UserModel.js';
+import ApiError from "../exception/ApiError.js";
+import User from "../model/UserModel.js";
 
 export default async function (req, res, next) {
   try {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log('адрессс', req.socket.remoteAddress);
+    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    console.log("адрессс", req.socket.remoteAddress);
     const regularValidMail = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
     const regularValidPhone = /^[\d\+][\d\(\)\ -]{4,14}\d$/;
-    const regularValidPassword =
-      /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,12}/g;
+    const regularValidPassword = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,12}/g;
     const { email, password } = req.body;
-    if (!email || !password) return next(ApiError.BadRequest('Заполните все поля ввода!'))
+    if (!email || !password) {
+      if (!email) {
+        errors.email.push("Поле обязательно для заполнения");
+      }
+      if (!password) {
+        errors.password.push("Поле обязательно для заполнения");
+      }
+      return next(ApiError.ValidationException("Ошибка валидации", errors));
+    }
     const validMail = regularValidMail.test(email);
     const validPass = regularValidPassword.test(password);
     const candidate = await User.findOne({ where: { email } });
     if (!validMail || !validPass || candidate) {
       const errors = { email: [], password: [] };
       if (!validMail) {
-        errors.email.push('Некорректный адрес электронной почты');
+        errors.email.push("Некорректный адрес электронной почты");
       }
       if (!validPass) {
         errors.password.push(
-          'Пароль должен содержать строчные, прописные буквы и цифры а так же быть не менее 6 и не более 12 символов длинной'
+          "Пароль должен содержать строчные, прописные буквы и цифры а так же быть не менее 6 и не более 12 символов длинной"
         );
       }
       if (candidate) {
@@ -29,7 +36,7 @@ export default async function (req, res, next) {
           `Пользователь с почтовым адресом ${email} уже существует`
         );
       }
-      return next(ApiError.ValidationException('Ошибка валидации', errors));
+      return next(ApiError.ValidationException("Ошибка валидации", errors));
     }
     return next();
   } catch (errors) {
