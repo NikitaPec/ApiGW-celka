fc06b16fd17cbfe5cce4b6b6f9d0d877d2f773b3
NikPecherkin <Nikpecherkin@gmail.com>
2023-01-29 22:13:23 +0300

    @bugfix/router/index.js /check-auth post=>get

--- router/index.js
+++ router/index.js
@@ -7,7 +7,7 @@ const router = new Router();
 router.post("/registration", validateMiddleWare, UserController.registration);
 router.post("/login", UserController.login);
 router.post("/logout", UserController.logout);
-router.post("/check-auth", authMiddleWare, UserController.checkAuth);
+router.get("/check-auth", authMiddleWare, UserController.checkAuth);
 router.get("/activate/:link", UserController.activate);
 router.get("/refresh", UserController.refresh);
 
